8e8145bac8baf1315da428f3fe447f94e14ed6f9
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-28 10:26:11 +0200

    Added POST route

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -33,3 +33,37 @@ export const GET: APIRoute = async ({ request }) => {
     });
   }
 };
+
+export const POST: APIRoute = async ({ request }) => {
+  try {
+    const options = {
+      method: "GET",
+      headers: { Authorization: `Bearer ${import.meta.env.COOLIFY_API_TOKEN}` },
+    };
+    const uuid = new URL(request.url).searchParams.get("uuid");
+
+    if (!uuid) throw new Error("no uuid");
+
+    fetch(
+      `https://${
+        import.meta.env.COOLIFY_DOMAIN
+      }/api/v1/deploy?uuid=${uuid}&force=false`,
+      options
+    )
+      .then((response) => {
+        if (response.status != 200) throw new Error("Bad response");
+      })
+      .catch(() => {
+        return new Response(JSON.stringify("server error"), {
+          status: 500,
+          statusText: "internal server error",
+        });
+      });
+    return new Response(JSON.stringify("Success, build in queue"));
+  } catch (err) {
+    return new Response(JSON.stringify(err.message), {
+      status: 400,
+      statusText: "bad request",
+    });
+  }
+};
