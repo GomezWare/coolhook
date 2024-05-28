933ea52f5e39f4aff16d174e1a9231e7f6be0a7f
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-28 09:33:47 +0200

    Configure scripts to run the servers in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  server: { port: 80 },
   adapter: node({
     mode: "standalone",
   }),
-  server: { port: 80 },
 });
